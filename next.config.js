c99a5fb6a4cbca6df21d40e1d02e905dfcaaf783
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2024-01-15 18:46:09 +0300

    upd

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "api.psytrance.ru",
-      },
-      {
-        protocol: "https",
-        hostname: "**.gravatar.com",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
